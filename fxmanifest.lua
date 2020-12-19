fb1c4bc43b9b768bd5204e10adbcd84505e1c39e
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2020-12-19 14:01:02 -0600

    Version Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'gta5' }
 
 author 'TrevorBarns w/ credits see GitHub'
 description 'A siren / emergency lights controller for FiveM.'
-version '3.1.3'
+version '3.1.4'
 
 ui_page('html/index.html')
 
