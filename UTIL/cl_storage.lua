f6fc3513c340859368f3750d3906540e38b43277
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2021-03-04 11:07:56 -0600

    Multi: Refactor for readability. Bug: change reset default values to settings

--- UTIL/cl_storage.lua
+++ UTIL/cl_storage.lua
@@ -2,7 +2,6 @@
 ---------------------------------------------------
 LUXART VEHICLE CONTROL V3 (FOR FIVEM)
 ---------------------------------------------------
-Last revision: FEBRUARY 23 2021 (VERS. 3.2.0)
 Coded by Lt.Caine
 ELS Clicks by Faction
 Additional Modification by TrevorBarns
@@ -35,7 +34,7 @@ RegisterCommand('lvcfactoryreset', function(source, args)
 		end
 		Storage:ResetSettings()
 		UTIL:Print("Success: cleared all save data.", true)
-		HUD:ShowNotification("~g~Success~s~: You have deleted all save data and reset LVC.")
+		HUD:ShowNotification("~g~Success~s~: You have deleted all save data and reset LVC.", true)
 	end
 end)
 ------------------------------------------------
@@ -71,7 +70,10 @@ function Storage:SaveSettings()
 	SetResourceKvp(save_prefix .. "save_version", Storage:GetCurrentVersion())
 	
 	--HUD Settings
-	local hud_save_data = { Show_HUD = HUD:GetHudState(), HUD_Scale = HUD:GetHudScale(), HUD_pos = HUD:GetHudPosition() }
+	local hud_save_data = { Show_HUD = HUD:GetHudState(),
+							HUD_Scale = HUD:GetHudScale(), 
+							HUD_pos = HUD:GetHudPosition() 
+						  }
 	SetResourceKvp(save_prefix .. "hud_data",  json.encode(hud_save_data))
 
 	--Profile Specific Settings
@@ -81,10 +83,10 @@ function Storage:SaveSettings()
 			local profile_save_data = {  PMANU = UTIL:GetToneID('PMANU'), 
 										 SMANU = UTIL:GetToneID('SMANU'),
 										 AUX   = UTIL:GetToneID('AUX'),
-										 airhorn_intrp = tone_airhorn_intrp,
+										 airhorn_intrp 		= tone_airhorn_intrp,
 										 main_reset_standby = tone_main_reset_standby,
-										 park_kill = park_kill,
-										 custom_tone_names = custom_tone_names,
+										 park_kill 			= park_kill,
+										 custom_tone_names 	= custom_tone_names,
 										 tone_options = json.encode(UTIL:GetToneOptionsTable()),
 									   }
 
@@ -102,25 +104,25 @@ function Storage:SaveSettings()
 			
 			--Audio Settings
 			local audio_save_data = {	button_sfx_scheme = button_sfx_scheme,
-										on_volume = on_volume,
-										off_volume = off_volume,
-										upgrade_volume = upgrade_volume,
-										downgrade_volume = downgrade_volume,
-										activity_reminder_volume = activity_reminder_volume,
-										hazards_volume = hazards_volume,
-										lock_volume = lock_volume,
-										lock_reminder_volume = lock_reminder_volume,
-										airhorn_button_SFX = airhorn_button_SFX,
-										manu_button_SFX = manu_button_SFX,
-										activity_reminder_index = activity_reminder_index,	
+										on_volume 					= on_volume,
+										off_volume 					= off_volume,
+										upgrade_volume 				= upgrade_volume,
+										downgrade_volume 			= downgrade_volume,
+										activity_reminder_volume 	= activity_reminder_volume,
+										hazards_volume 				= hazards_volume,
+										lock_volume 				= lock_volume,
+										lock_reminder_volume 		= lock_reminder_volume,
+										airhorn_button_SFX 			= airhorn_button_SFX,
+										manu_button_SFX 			= manu_button_SFX,
+										activity_reminder_index 	= activity_reminder_index,	
 									}						
 			SetResourceKvp(save_prefix.."profile_"..profile_name.."_audio_data",  json.encode(audio_save_data))
 			UTIL:Print("LVC:STORAGE: saving profile_"..profile_name.."_audio_data")
 		else
-			HUD:ShowNotification("~b~LVC: ~r~ SAVE ERROR~s~: profile_name after gsub is nil.")
+			HUD:ShowNotification("~b~LVC: ~r~ SAVE ERROR~s~: profile_name after gsub is nil.", true)
 		end
 	else
-		HUD:ShowNotification("~b~LVC: ~r~SAVE ERROR~s~: UTIL:GetVehicleProfileName() returned nil.")
+		HUD:ShowNotification("~b~LVC: ~r~SAVE ERROR~s~: UTIL:GetVehicleProfileName() returned nil.", true)
 	end
 end
 
@@ -159,55 +161,58 @@ function Storage:LoadSettings()
 					UTIL:SetToneByID('PMANU', profile_save_data.PMANU)
 					UTIL:SetToneByID('SMANU', profile_save_data.SMANU)
 					UTIL:SetToneByID('AUX', profile_save_data.AUX)
-					tone_airhorn_intrp = profile_save_data.airhorn_intrp
-					tone_main_reset_standby = profile_save_data.main_reset_standby
-					park_kill = profile_save_data.park_kill
-					custom_tone_names = profile_save_data.custom_tone_names
-					local tone_options = json.decode(profile_save_data.tone_options)
-						if tone_options ~= nil then
-							for tone_id, option in pairs(tone_options) do
-								if SIRENS[tone_id] ~= nil then
-									SIRENS[tone_id].Option = option
+					if main_siren_settings_masterswitch then
+						tone_airhorn_intrp 		= profile_save_data.airhorn_intrp
+						tone_main_reset_standby = profile_save_data.main_reset_standby
+						park_kill 				= profile_save_data.park_kill
+						custom_tone_names 		= profile_save_data.custom_tone_names
+						local tone_options = json.decode(profile_save_data.tone_options)
+							if tone_options ~= nil then
+								for tone_id, option in pairs(tone_options) do
+									if SIRENS[tone_id] ~= nil then
+										SIRENS[tone_id].Option = option
+									end
 								end
 							end
-						end
+					end
 					UTIL:Print("LVC:STORAGE: loaded "..profile_name..".")
 				end
 			
-			
-				if custom_tone_names then
-					local tone_names = GetResourceKvpString(save_prefix.."profile_"..profile_name.."_tone_names")
-					if tone_names ~= nil then
-						tone_names = json.decode(tone_names)
-						for i, name in pairs(tone_names) do
-							if SIRENS[i] ~= nil then
-								SIRENS[i].Name = name
+				if main_siren_settings_masterswitch then
+					if custom_tone_names then
+						local tone_names = GetResourceKvpString(save_prefix.."profile_"..profile_name.."_tone_names")
+						if tone_names ~= nil then
+							tone_names = json.decode(tone_names)
+							for i, name in pairs(tone_names) do
+								if SIRENS[i] ~= nil then
+									SIRENS[i].Name = name
+								end
 							end
 						end
+						UTIL:Print("LVC:STORAGE: loaded "..profile_name.." custom tone names.")
 					end
-					UTIL:Print("LVC:STORAGE: loaded "..profile_name.." custom tone names.")
 				end
 			
 				--Audio Settings 
 				local audio_save_data = GetResourceKvpString(save_prefix.."profile_"..profile_name.."_audio_data")
 				if audio_save_data ~= nil then
 					audio_save_data = json.decode(audio_save_data)
-					button_sfx_scheme = audio_save_data.button_sfx_scheme
-					on_volume = audio_save_data.on_volume
-					off_volume = audio_save_data.off_volume
-					upgrade_volume = audio_save_data.upgrade_volume
-					downgrade_volume = audio_save_data.downgrade_volume
-					activity_reminder_volume = audio_save_data.activity_reminder_volume
-					hazards_volume = audio_save_data.hazards_volume
-					lock_volume = audio_save_data.lock_volume
-					lock_reminder_volume = audio_save_data.lock_reminder_volume
-					airhorn_button_SFX = audio_save_data.airhorn_button_SFX
-					manu_button_SFX = audio_save_data.manu_button_SFX
-					activity_reminder_index = audio_save_data.activity_reminder_index
+					button_sfx_scheme 			= audio_save_data.button_sfx_scheme
+					on_volume 					= audio_save_data.on_volume
+					off_volume 					= audio_save_data.off_volume
+					upgrade_volume 				= audio_save_data.upgrade_volume
+					downgrade_volume 			= audio_save_data.downgrade_volume
+					activity_reminder_volume 	= audio_save_data.activity_reminder_volume
+					hazards_volume 				= audio_save_data.hazards_volume
+					lock_volume 				= audio_save_data.lock_volume
+					lock_reminder_volume 		= audio_save_data.lock_reminder_volume
+					airhorn_button_SFX 			= audio_save_data.airhorn_button_SFX
+					manu_button_SFX 			= audio_save_data.manu_button_SFX
+					activity_reminder_index 	= audio_save_data.activity_reminder_index
 					UTIL:Print("LVC:STORAGE: loaded audio data.")
 				end
 			else
-				HUD:ShowNotification("~b~LVC:~r~ LOADING ERROR~s~: profile_name after gsub is nil.")
+				HUD:ShowNotification("~b~LVC:~r~ LOADING ERROR~s~: profile_name after gsub is nil.", true)
 			end
 		end
 	end
@@ -240,16 +245,16 @@ function Storage:ResetSettings()
 	activity_reminder_index = 1
 	last_activity_timer = 0
 
-	button_sfx_scheme = default_sfx_scheme_name
 	button_sfx_scheme_id = 1
-	on_volume = default_on_volume	
-	off_volume = default_off_volume	
-	upgrade_volume = default_upgrade_volume	
-	downgrade_volume = default_downgrade_volume
-	hazards_volume = default_hazards_volume
-	lock_volume = default_lock_volume
-	lock_reminder_volume = default_lock_reminder_volume
-	activity_reminder_volume = default_reminder_volume
+	button_sfx_scheme 			= default_sfx_scheme_name
+	on_volume 					= default_on_volume	
+	off_volume 					= default_off_volume	
+	upgrade_volume 				= default_upgrade_volume	
+	downgrade_volume 			= default_downgrade_volume
+	hazards_volume 				= default_hazards_volume
+	lock_volume 				= default_lock_volume
+	lock_reminder_volume 		= default_lock_reminder_volume
+	activity_reminder_volume 	= default_reminder_volume
 end
 
 ------------------------------------------------
@@ -284,7 +289,7 @@ function IsNewerVersion(version, test_version)
 	elseif s1 < c1 then
 		return false
 	else
-		if s2 > c2 then			-- s1.0.0 Vs c1.0.0
+		if s2 > c2 then			-- 0.s2.0 Vs 0.c2.0
 			return true
 		elseif s2 < c2 then
 			return false
