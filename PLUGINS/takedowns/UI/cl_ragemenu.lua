3be1660258bfa527064ec7fd6b89de0189355ae9
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2021-03-04 13:26:32 -0600

    Remove ability to toggle TKD functionality per player.

--- PLUGINS/takedowns/UI/cl_ragemenu.lua
+++ PLUGINS/takedowns/UI/cl_ragemenu.lua
@@ -30,41 +30,33 @@ end
 Citizen.CreateThread(function()
     while true do
 		--TKD SETTINGS
-		RageUI.IsVisible(RMenu:Get('lvc', 'tkdsettings'), function()
-			RageUI.Checkbox('Enabled', "Toggles takedown light functionality. All vehicles: your own and others.", tkd_masterswitch, {}, {
-            onChecked = function()
-				tkd_masterswitch = true
-            end  ,          
-			onUnChecked = function()
-				tkd_masterswitch = false
-            end
-            })				
-			RageUI.List('Integration', {"Off", "TKDs Set High-beams", "High-beams Set TKDs"}, tkd_mode, "Determines whether high-beams will auto toggle takedowns or visa versa.", {}, tkd_masterswitch, {
+		RageUI.IsVisible(RMenu:Get('lvc', 'tkdsettings'), function()			
+			RageUI.List('Integration', {"Off", "TKDs Set High-beams", "High-beams Set TKDs"}, tkd_mode, "Determines whether high-beams will auto toggle take-downs or visa versa.", {}, true, {
 			  onListChange = function(Index, Item)
 				tkd_mode = Index
 			  end,
 			})			
-			RageUI.List('Position', {"1", "2", "3", "4"}, tkd_scheme, "Select predefined positions of light source.", {}, tkd_masterswitch, {
+			RageUI.List('Position', {"1", "2", "3", "4"}, tkd_scheme, "Select predefined positions of light source.", {}, true, {
 			  onListChange = function(Index, Item)
 				tkd_scheme = Index
 			  end,
 			})
-			RageUI.Slider('Intensity', tkd_intensity, 150, 15, "Set brightness/intensity of takedowns.", false, {}, tkd_masterswitch, {
+			RageUI.Slider('Intensity', tkd_intensity, 150, 15, "Set brightness/intensity of take-downs.", false, {}, true, {
 			  onSliderChange = function(Index)
 				tkd_intensity = Index
 			  end,	  
 			})					
-			RageUI.Slider('Radius', tkd_radius, 90, 9, "Set width of takedowns.", false, {}, tkd_masterswitch, {
+			RageUI.Slider('Radius', tkd_radius, 90, 9, "Set width of take-downs.", false, {}, true, {
 			  onSliderChange = function(Index)
 				tkd_radius = Index
 			  end,	  
 			})			
-			RageUI.Slider('Distance', tkd_distance, 250, 25, "Set the max distance the takedowns can travel.", false, {}, tkd_masterswitch, {
+			RageUI.Slider('Distance', tkd_distance, 250, 25, "Set the max distance the take-downs can travel.", false, {}, true, {
 			  onSliderChange = function(Index)
 				tkd_distance = Index
 			  end,	  
 			})				
-			RageUI.Slider('Falloff', tkd_falloff, 2000, 200, "Set how fast light \"falls off\" or appears dim.", false, {}, tkd_masterswitch, {
+			RageUI.Slider('Falloff', tkd_falloff, 2000, 200, "Set how fast light \"falls off\" or appears dim.", false, {}, true, {
 			  onSliderChange = function(Index)
 				tkd_falloff = Index
 			  end,	  
