bcc832dd55a9c237b8f43640b058cbb20f9cd419
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2021-03-07 13:00:50 -0600

    Fix: plugins menu missing closure

--- PLUGINS/cl_plugins.lua
+++ PLUGINS/cl_plugins.lua
@@ -16,28 +16,29 @@ settings. Handles Plugin -> LVC event communication
 Citizen.CreateThread(function()
     while plugins_installed do
 	    RageUI.IsVisible(RMenu:Get('lvc', 'plugins'), function()
-		-----------------------------------------------------------------------------------------------------------------
-		if smart_siren_masterswitch then
-			RageUI.Button('Smart Siren Settings', "Open smart siren settings menu. (smart_sirens)", {RightLabel = "→→→"}, true, {
-			  onSelected = function()
-			  end,
-			}, RMenu:Get('lvc', 'smartsiren'))	
-		end
-		-----------------------------------------------------------------------------------------------------------------
-		if tkd_masterswitch then
-			RageUI.Button('Takedown Settings', "Open takedown lights menu. (takedowns)", {RightLabel = "→→→"}, true, {
-			  onSelected = function()
-			  end,
-			}, RMenu:Get('lvc', 'tkdsettings'))	
-		end
-		-----------------------------------------------------------------------------------------------------------------
-		if ei_masterswitch then
-			RageUI.Button('Extra Integration Settings', "Open extra integration menu. (extra_integration)", {RightLabel = "→→→"}, true, {
-			  onSelected = function()
-			  end,
-			}, RMenu:Get('lvc', 'extrasettings'))	
-		end		
-		-----------------------------------------------------------------------------------------------------------------
+			-----------------------------------------------------------------------------------------------------------------
+			if smart_siren_masterswitch then
+				RageUI.Button('Smart Siren Settings', "Open smart siren settings menu. (smart_sirens)", {RightLabel = "→→→"}, true, {
+				  onSelected = function()
+				  end,
+				}, RMenu:Get('lvc', 'smartsiren'))	
+			end
+			-----------------------------------------------------------------------------------------------------------------
+			if tkd_masterswitch then
+				RageUI.Button('Takedown Settings', "Open takedown lights menu. (takedowns)", {RightLabel = "→→→"}, true, {
+				  onSelected = function()
+				  end,
+				}, RMenu:Get('lvc', 'tkdsettings'))	
+			end
+			-----------------------------------------------------------------------------------------------------------------
+			if ei_masterswitch then
+				RageUI.Button('Extra Integration Settings', "Open extra integration menu. (extra_integration)", {RightLabel = "→→→"}, true, {
+				  onSelected = function()
+				  end,
+				}, RMenu:Get('lvc', 'extrasettings'))	
+			end		
+			-----------------------------------------------------------------------------------------------------------------
+		end)
         Citizen.Wait(0)
 	end
 end)
