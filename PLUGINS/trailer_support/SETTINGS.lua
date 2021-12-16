41eea93542962ca04331559cdd5bafc8f24513ff
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2021-12-16 13:02:13 -0600

    Refactor: P-TS support hyperlink to prevent appending comment termination ]]

--- PLUGINS/trailer_support/SETTINGS.lua
+++ PLUGINS/trailer_support/SETTINGS.lua
@@ -2,6 +2,6 @@
 trailer_masterswitch = true
 --	Determines if trailer_support plugin can be activated.
 
---[[ Documentation / Wiki: https://github.com/TrevorBarns/luxart-vehicle-control/wiki/Trailer-Support]]
+--[[ Documentation / Wiki: https://github.com/TrevorBarns/luxart-vehicle-control/wiki/Trailer-Support ]]
 
 TRAILERS = { }
