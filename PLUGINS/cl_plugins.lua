ec0670fc7f7b8e82ef297206376a2395c591572e
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2022-05-20 16:42:00 -0500

    Remove: smart_siren plugin code till development continues

--- PLUGINS/cl_plugins.lua
+++ PLUGINS/cl_plugins.lua
@@ -30,13 +30,6 @@ CreateThread(function()
     while plugins_installed do
 	    RageUI.IsVisible(RMenu:Get('lvc', 'plugins'), function()
 			-----------------------------------------------------------------------------------------------------------------
-			if smart_siren_masterswitch then
-				RageUI.Button('Smart Siren Settings', 'Open smart siren settings menu. (smart_sirens)', {RightLabel = '→→→'}, true, {
-				  onSelected = function()
-				  end,
-				}, RMenu:Get('lvc', 'smartsiren'))	
-			end
-			-----------------------------------------------------------------------------------------------------------------
 			if tkd_masterswitch then
 				RageUI.Button('Takedown Settings', 'Open takedown lights menu. (takedowns)', {RightLabel = '→→→'}, true, {
 				  onSelected = function()
