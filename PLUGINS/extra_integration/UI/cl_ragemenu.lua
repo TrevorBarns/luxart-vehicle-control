725f7f43e584980dfcecd64bb52ff78736e156ad
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2022-08-21 21:27:21 -0500

    P-Fix: broken autopark EI menu

--- PLUGINS/extra_integration/UI/cl_ragemenu.lua
+++ PLUGINS/extra_integration/UI/cl_ragemenu.lua
@@ -13,12 +13,10 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ---------------------------------------------------
@@ -40,7 +38,7 @@ CreateThread(function()
             end
             })
 
-			RageUI.List(Lang:t('plugins.ei_auto_park'), {'Off', '1/2', '1', '5'}, EI:GetParkTimeIndex(), Lang:t('plugins.ei_auto_park_desc'), {timer = ("%1.0f"):format((EI:GetStoppedTimer() / 1000) or 0)}, {}, true, {
+			RageUI.List(Lang:t('plugins.ei_auto_park'), {'Off', '1/2', '1', '5'}, EI:GetParkTimeIndex(), Lang:t('plugins.ei_auto_park_desc', {timer = ("%1.0f"):format((EI:GetStoppedTimer() / 1000) or 0)}), {}, true, {
 			  onListChange = function(Index, Item)
 				if Index > 1 then
 					EI:SetParkTimeIndex(Index)
