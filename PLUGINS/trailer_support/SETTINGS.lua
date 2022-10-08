81570bb327e9bb87fb6cccc8849aa3ad4f47e71c
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2022-10-08 17:33:40 -0500

    Move plugins to extras repository

--- PLUGINS/trailer_support/SETTINGS.lua
+++
@@ -1,7 +0,0 @@
---------------------TRAILER SUPPORT SETTINGS---------------------
-trailer_masterswitch = false
---	Determines if trailer_support plugin can be activated.
-
---[[ Documentation / Wiki: https://github.com/TrevorBarns/luxart-vehicle-control/wiki/Trailer-Support ]]
-
-TRAILERS = { }
